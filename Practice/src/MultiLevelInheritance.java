b8009400950b7478320c416d1dac49a7c0d6e390
Amalin Delver <amalinam1011@gmail.com>
2024-02-06 19:34:15 +0530

    First

---
+++ Practice/src/MultiLevelInheritance.java
@@ -0,0 +1,33 @@
+public class MultiLevelInheritance {
+    public static void main(String[] args) {
+        volume v = new volume();
+        v.initialize();
+        v.volume();
+        v.area();
+    }
+}
+
+class rectangle {
+    int l, w, h;
+
+    public void initialize() {
+        l = 10;
+        w = 25;
+        h = 12;
+    }
+}
+
+class area extends rectangle {
+    public void area() {
+        int a1 = l * w;
+        System.out.print("Area of rectangle is l*w" + a1);
+    }
+}
+
+class volume extends area {
+    public void volume() {
+        int b1 = l * w * h;
+        System.out.println("Volume of rectangle is l*w*h" + b1);
+    }
+}
+
